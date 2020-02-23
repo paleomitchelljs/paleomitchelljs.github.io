deb110cc31724f371b84761d379192d5f367e30f
Jon <jonsmitch2@gmail.com>
2020-02-22 22:02:39 -0500

    code w/ color

--- code/reformatData07.R
+++ code/reformatData07.R
@@ -50,3 +50,8 @@ for (i in 3:ncol(overallFreq))	{
 alleleFreqs <- data.frame(year=yearVec, locus=locus, freq=alleleVec, rfreq=scale_alleleVec, d_freq = dOverVec, d_imm = dImmVec, d_surv = dSurvVec, d_birth = dBirthVec, n_freq = nOverVec, n_imm = nImmVec, n_surv = nSurvVec, n_birth = nBirthVec)
 sizeChanges <- data.frame(year = overallN[,1], n_total = overallN[,3], n_births = c(NA,nestlingsN[,3]), n_survive = c(NA,survivorN[,3]), n_immigrants = c(NA,immN[,3]))
 
+# Set up plots
+Alpha <- 0.75
+divCols <- rev(c(rgb(158/255,1/255,66/255, Alpha),rgb(213/255,62/255,79/255, Alpha),rgb(244/255,109/255,67/255, Alpha),rgb(253/255,174/255,97/255, Alpha),rgb(254/255,224/255,139/255, Alpha),rgb(255/255,255/255,191/255, Alpha),rgb(230/255,245/255,152/255, Alpha),rgb(171/255,221/255,164/255, Alpha),rgb(102/255,194/255,165/255, Alpha),rgb(50/255,136/255,189/255, Alpha),rgb(94/255,79/255,162/255, Alpha)))
+qualCols <- c(rgb(166/255,206/255,227/255,Alpha),rgb(31/255,120/255,180/255,Alpha),rgb(178/255,223/255,138/255,Alpha),rgb(51/255,160/255,44/255,Alpha),rgb(251/255,154/255,153/255,Alpha),rgb(227/255,26/255,28/255,Alpha),rgb(253/255,191/255,111/255,Alpha),rgb(255/255,127/255,0,Alpha),rgb(202/255,178/255,214/255,Alpha))
+Pal <- colorRampPalette(divCols, interpolate = "spline", alpha = T)
