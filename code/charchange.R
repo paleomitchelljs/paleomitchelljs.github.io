81b6f910ebe515fed1c7984e153143dad00e1431
Jon <jonsmitch2@gmail.com>
2021-02-16 14:59:36 -0500

    update charchange.R

--- code/charchange.R
+++ code/charchange.R
@@ -1,10 +1,10 @@
-makeCharMat <- function(tree, traitMatrix) {
+### currently cannot handle ambiguity
+
+makeCharMat <- function(tree, traitMatrix, ambig="?") {
 	require(phangorn)
 	require(paleotree)
 
-	# this specific approach has only been tested for binary states, but should be extendible
-	# but there may need to be adjustments
-	# traitMatrix also needs to have rownames == tiplabels of tree
+	# traitMatrix needs to have rownames == tiplabels of tree
 	traitMatrix <- traitMatrix[tree$tip.label,]
 
 	# make a storage matrix
@@ -15,23 +15,25 @@ makeCharMat <- function(tree, traitMatrix) {
 		names(Trait) <- rownames(traitMatrix)
 
 		# use paleotree to find states
-		# can probably replace the "1" with a which() applied to rows 
 		aTrait <- ancPropStateMat(Trait, tree, type="ACCTRAN")
-		charMat[,i] <-  apply(aTrait, 1, function(x) which(x == 1))
+
+		# ambiguously reconstructed traits are currently randomly sampled.
+		charMat[,i] <-  apply(aTrait, 1, function(x) ifelse(min(x)==0, which(x == 0), sample(x,1,prob=x)))
 	}
 	rownames(charMat) <- rownames(aTrait)
 	return(charMat)
 }
 #
-plotCharChange <- function(tree, charMat, plottree=TRUE, Pch=25, Bg="lightblue", Rescale=FALSE, Rmin = 0.5, Mar=c(4,4,4,4))	{
+plotCharChange <- function(tree, charMat, plottree=TRUE, Pch=c(24, 25), Bg=c("blue", "red"), Rescale=FALSE, Rmin = 0.5, Mar=c(4,4,4,4), Threshold = T)	{
 	# tree is a phyogenetic tree w/ branch lengths
 	# charMat is a matrix with character states for each tip AND each node. Must have rownames equalling the edge number for the phylo object of ape
 	# plottree is a logical, set to TRUE to plot the tree
 	# Pch and Bg both control the points that plot alongside the char change notations
+	#	Pch and Bg can be set to the same value (eg., Pch=c(25,25), Bg=c("lightblue","lightblue")) to map all transitions identically. Currently the only "custom" points this allows are "up" and "down" transitions, although it's R. Anything is possible!
 	# Mar are the (bottom, left, top, right) margin values for R.
 	# Rescale is a logical, asking if you want to make a tree with branches rescaled to the observed change numbers. Rmin is the min branch length for a rescaled tree
 	#		for a lot of characters, you probably want to do x <- plotCharChange(tree,Data,Rescale=T) followed by plotCharChange(x, Data)
-
+	# Threshold is a limit to prevent the reconstruction of ambiguous ransitions. If set to false, transitions from "0.5 -> X" will be mapped. If set T, only confidently reconstructed transitions will be mapped. Neither is optimal, and many better solutions are obvious, but this is what I've done for now.
 	require(ape)
 	# extract edge matarix
 	eMat <- tree$edge
@@ -58,7 +60,11 @@ plotCharChange <- function(tree, charMat, plottree=TRUE, Pch=25, Bg="lightblue",
 	for (i in 1:nrow(eMat))	{
 		# find which characters changed (which ancestor states != descendent states for each edge)
 		changeVec <- Ancest[i,] - Descend[i,]
+		if (Threshold == TRUE)	{
+			changeVec <- sapply(changeVec, floor)
+		}
 		Chars <- which(abs(changeVec) > 0)
+		CharVals <- as.numeric(changeVec[Chars] > 0)+1
 		# if any change happened..
 		if (length(Chars) > 0)	{
 			Reformat <- sapply(Chars, function(x) paste(x, ": ", Ancest[i,x], "->", Descend[i,x], sep=""))
@@ -80,11 +86,11 @@ plotCharChange <- function(tree, charMat, plottree=TRUE, Pch=25, Bg="lightblue",
 			# add the change on the tree on the right branch
 			for (j in 1:length(Chars))	{
 				if (lastPP$direction %in% c("rightwards", "leftwards"))	{
-					points(xloc_b+(j*xRange), yloc, pch=Pch, bg=Bg)
+					points(xloc_b+(j*xRange), yloc, pch=Pch[CharVals[j]], bg=Bg[CharVals[j]])
 					text(xloc_b+(j*xRange), yloc, Reformat[j], pos=4, srt=270, offset=1, xpd=NA)
 				}
 				else	{
-					points(xloc, yloc_b+(j*yRange), pch=Pch, bg=Bg)
+					points(xloc, yloc_b+(j*yRange), pch=Pch[CharVals[j]], bg=Bg[CharVals[j]])
 					text(xloc, yloc_b+(j*yRange), Reformat[j], pos=4, xpd=NA)				
 				}		
 			}
@@ -102,30 +108,31 @@ plotCharChange <- function(tree, charMat, plottree=TRUE, Pch=25, Bg="lightblue",
 #      Make the plot!        #
 ##############################
 # make fake data
-library(phangorn)
 library(phytools)
-s <- "owls(((Strix_aluco:4.2,Asio_otus:4.2):3.1,Athene_noctua:7.3):6.3,Tyto_alba:13.5);"
-testT <- read.tree(text=s)
-testChar <- rbind(
-			c(1,0,1,1,0), 
-			c(1,1,0,1,0), 
-			c(0,0,1,1,1), 
-			c(1,1,0,1,0)
-			)
-rownames(testChar) <- testT$tip.label
+library(paleotree)
 
+# make a fake tree
+simphy <- pbtree(n=10)
 
-# phangorn is odd, so makeCharMat() will likely need heavy editting for different datasets
-# but you do not need to use phangorn.
-# Any data you can arrange into a matrix like charMat *will work* for plotCharChange. 
+# make some fake traits
+fakeTraits <- matrix(0, nrow=Ntip(simphy), ncol=10)
+for (i in 1:ncol(fakeTraits))	{
+	traits <- sim.history(simphy, Q=matrix(rep(1/5,25), ncol=5, nrow=5))
+	exTraits <- as.numeric(sapply(traits$maps, function(x) names(x)[which(x == max(x))])[1:Ntip(simphy)])
+	renum <- seq(0, length(unique(exTraits)), 1)
+	names(renum) <- unique(exTraits)
+	fakeTraits[,i] <- renum[as.character(exTraits)]
+}
+rownames(fakeTraits) <- simphy$tip.label
+
+# Any data you can arrange into a matrix like charMat *will work* for plotCharChange.
 # charMat is a matrix where each row is an edge number, each column is a trait, and each cell is a reconstruction
 # can be from acctran, or simmap, or ace, or whatever
-# It (currently) has to be in standard R phylo object edge ordering. That is, the first 1 to Ntip taxa are tips, then the nodes follow after
-Mat <- makeCharMat(testT, testChar)
-
+# It (currently) has to be in standard R phylo object edge ordering. That is, the first 1 to Ntip taxa are tips, then the nodes follow after.
+Mat <- makeCharMat(simphy, fakeTraits)
 
 # make the plot
-x <- plotCharChange(testT, Mat, Rescale=T)
+x <- plotCharChange(simphy, Mat, Rescale=T)
 
 # make a plot w/ branches scaled to num char changes
 plotCharChange(x, Mat)
