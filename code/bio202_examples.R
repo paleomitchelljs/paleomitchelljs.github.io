079b763ca7a7663f90a79c60a8e79d4472aae67d
Jon Mitchell <jonsmitch2@gmail.com>
2025-09-24 12:13:25 -0500

    ibd sims

--- code/bio202_examples.R
+++ code/bio202_examples.R
@@ -1,3 +1,7 @@
+##############################
+######### gene drift #########
+##############################
+
 ######## generate individual
 gen_ind <- function(alleles, freq = NULL, ploidy = 2)	{
 	if (is.null(freq))	{
@@ -86,6 +90,67 @@ plot_pop <- function(Pop, Col = "red", Col2 = "blue", Lwd = 2)	{
 }
 
 
-#test <- sim_pop(time = 200, N = 40)
+#test <- sim_pop(time = 500, N = 50)
 #plot_pop(test)
 #z <-  c(68, 39, 59, 86, 124, 72, 44, 65, 46, 43, 54, 82, 95, 112, 40, 87, 78, 64, 95, 133, 68, 104, 146)
+
+##############################
+######## gene dropper ########
+##############################
+### THIS IS FOR MRCA / GENE DROP / ETC SECTION
+init_pop <- function(N, nloci = 10)	{
+	Gen0 <- array(data = sapply(1:N, function(x) rep(x, 2*nloci)), dim = c(nloci, 2, N))
+	Gen1 <- replicate(N, matrix(sort(rep(sample(1:N, 2), nloci)), ncol=2))
+	return(list(Gen0, Gen1))
+}
+
+recomb <- function(x)	{
+	x <- cbind(x,x)
+	Loc <- sample(1:nrow(x), 1)
+	if (Loc > 1)	{
+		new1 <- c(x[1:(Loc-1),1], x[Loc:nrow(x),2])
+		new2 <- c(x[1:(Loc-1),2], x[Loc:nrow(x),1])
+		x[,1] <- new1
+		x[,2] <- new2	
+	}
+	return(x)
+}
+
+matetwo <- function(x1, x2)	{
+	gamete1 <- recomb(x1)[,sample(1:4,1)]
+	gamete2 <- recomb(x2)[,sample(1:4,1)]
+	kid <- cbind(gamete1, gamete2)
+	return(kid)
+}
+
+adv_gen <- function(Pop)	{
+	Start <- Pop[[length(Pop)]]
+	nextGen <- array(0, dim = dim(Start))
+	for (i in 1:dim(Start)[3])	{
+		Parents <- sample(1:dim(Start)[3], 2)
+		nextGen[,,i] <- matetwo(Start[,,Parents[1]], Start[,,Parents[2]])
+	}
+	Pop[[length(Pop) + 1]] <- nextGen
+	return(Pop)
+}
+
+
+run_pop <- function(ngen, N = 5, nloci = 10)	{
+	Pop0 <- init_pop(N, nloci)
+	Pop <- Pop0
+	for (z in 1:ngen)	{
+		Pop <- adv_gen(Pop)
+	}
+	return(Pop)
+}
+
+plotPop <- function(Ngen = 350, Npop = 35, Nloci = 10)	{
+	test <- run_pop(Ngen, N = Npop, Nloci)
+	par(mfrow=c(1,2), las = 1, mar = c(4, 5, 1, 1), mgp = c(1.5, 0.5, 0), tck = -0.01)
+	plot(sapply(lapply(test, function(x) table(x)), length), type = "l", ylab = "num. original still around", ylim = c(0, Npop), xlab = "generation")
+	Cols <- c("#a6cee3","#1f78b4","#b2df8a","#33a02c","#fb9a99","#e31a1c","#fdbf6f","#ff7f00", "#cab2d6", "#6a3d9a")
+	byLocus <- do.call(rbind, lapply(test, function(x) apply(x, 1, function(y) length(unique(as.vector(y))))))
+	plot(1, 1, type="n", xlim = c(0, Ngen), ylim = c(0, Npop), xlab = "generation", ylab = "n alleles / locus")
+	silent <- sapply(1:ncol(byLocus), function(x) lines(1:nrow(byLocus), byLocus[,x], col = Cols[x], lwd = 2))
+	legend("topright", legend = "each color is a distinct locus", bty = "n")
+}
